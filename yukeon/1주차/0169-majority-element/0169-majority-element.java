0f673306e54b3d660d63fadacf647c937955e3eb
youKeon <yukeon97@gmail.com>
2023-09-13 00:33:58 +0900

    docs : 1주차 추가

---
+++ yukeon/1주차/0169-majority-element/0169-majority-element.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int majorityElement(int[] nums) {
+        int count = 0;
+        int ans = 0;
+        
+        for (int i : nums) {
+            if (count == 0) {
+                ans = i;
+            }
+            
+            if (ans == i) count++;
+            else count--;
+        }
+        return ans;
+    }
+}
