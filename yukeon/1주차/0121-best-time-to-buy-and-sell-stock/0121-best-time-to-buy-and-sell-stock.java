2992c2964faf2214dbb40b2d3797d19b10ea557c
youKeon <yukeon97@gmail.com>
2023-09-16 01:55:59 +0900

    121번 풀이

--- yukeon/1주차/0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
+++ yukeon/1주차/0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
@@ -1,18 +1,14 @@
 class Solution {
     public int maxProfit(int[] prices) {
-        int lsf = Integer.MAX_VALUE;
-        int op = 0;
-        int pist = 0;
-        
-        for(int i = 0; i < prices.length; i++){
-            if(prices[i] < lsf){
-                lsf = prices[i];
-            }
-            pist = prices[i] - lsf;
-            if(op < pist){
-                op = pist;
-            }
+        int cur = prices[0];
+        int ans = 0;
+
+        for(int i = 1; i < prices.length; i++) {
+            int tmp = prices[i] - cur;
+
+            if (tmp < 0) cur = prices[i];
+            else ans = Math.max(ans, tmp);
         }
-        return op;
+        return ans;
     }
 }
