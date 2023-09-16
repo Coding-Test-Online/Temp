3bad90f13eb60b8ea5502f1c14dde5617c7b4511
youKeon <yukeon97@gmail.com>
2023-09-16 19:39:27 +0900

    55번 풀이

--- yukeon/1주차/0055-jump-game/0055-jump-game.java
+++ yukeon/1주차/0055-jump-game/0055-jump-game.java
@@ -1,13 +1,18 @@
 class Solution {
     public boolean canJump(int[] nums) {
+        // step 1. 현재 인덱스에서 갈 수 있는 최대값 계산
+
+        // step 2. 최대값이 배열의 끝인 경우 true를 반환
+
+        // step 3. 최대값이 현재 인덱스 위치(== 0)이면 false
+        if (nums.length == 1) return true;
         int end = 0;
-        for (int i = 0; i < nums.length; i++){
-            int step = nums[i];
-            end = Math.max(i + step, end);
-            if (end >= nums.length - 1){
-                return true;
-            }
-            else if (end == i) return false;
+
+        for (int i = 0; i < nums.length; i++) {
+            end = Math.max(end, i + nums[i]);
+
+            if (end == i) return false;
+            else if (end >= nums.length - 1) return true; 
         }
         return true;
     }
